192dab48b8c6f31ce781d2e19f0fa3d7cb2fe617
liptonRoy <ujjjwal@gmail.com>
2016-08-04 10:41:02 +0600

    Fix code so that "index.php" shows all tests as "Passed" and in green
    implement deposit method in BankAccount class to passed deposit test
    implement withdraw method in BankAccount class to passed withdraw test
    implement transfer method in BankAccount class to passed transfer test
    throw an Exception in withdraw method in BankAccount class to passed
    withdraw too much test

--- classes/BankAccount.php
+++ classes/BankAccount.php
@@ -18,15 +18,27 @@ class BankAccount implements IfaceBankAccount
     public function deposit(Money $amount)
     {
         //implement this method
+		$this->balance = (int) (string) $this->balance + $amount->value() ;
+		
+		return $this->balance;
     }
 	
 	public function withdraw(Money $amount)
     {
-	
+		//implement this method
+		if ((int) (string) $this->balance < $amount->value()) {
+			throw new Exception("Withdrawl amount larger than balance");
+		}
+		
+		$this->balance = (int) (string) $this->balance - $amount->value() ;
+		
+		return $this->balance;
 	}
 
     public function transfer(Money $amount, BankAccount $account)
     {
         //implement this method
+		$this->balance = $this->withdraw($amount);
+		$account->balance = $account->deposit($amount);
     }
 }
